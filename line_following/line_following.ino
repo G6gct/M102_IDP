ead6b2768bfc1c46ef258da9cd8abff54a0036c5
G6gct <gct31@cam.ac.uk>
2023-10-17 22:37:54 +0100

    vito to add exisiting code

--- line_following/line_following.ino
+++ line_following/line_following.ino
@@ -1,3 +1,15 @@
+int cases = 0;
+int delay_time = 20;
+
+// Define different cases
+#define ON_LINE 0
+#define LINE_ON_LEFT 1       
+#define LINE_ON_RIGHT 2
+
+// Defining directions
+#define RIGHT 1
+#define LEFT -1
+
 // Define  pins for line sensors
 const int LINE_SENSOR_VLEFT = 5; 
 const int LINE_SENSOR_LEFT = 6; 
@@ -20,12 +32,37 @@ void LFSensorRead() {
 //3 of them at junction TODO: Code the 3 1110
 // change to junction mode:; which junction at
 }
+
 void setup() {
   Serial.begin(9600);
   // something something
 
 }
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void LineFollowStart() {
+  if(    (LFSensorReading[1]== 1 )&&(LFSensorReading[2]== 0 ))  {cases = LINE_ON_RIGHT;} 
+  else if((LFSensorReading[1]== 0 )&&(LFSensorReading[2]== 0 ))  {cases = ON_LINE;}
+  else if((LFSensorReading[1]== 0 )&&(LFSensorReading[2]== 1 ))  {cases = LINE_ON_LEFT;}
+}
+
+void followLine(void) {
+  LFSensorRead(); // calls the sensor reading
+  LineFollowStart();  // calls the line sensors 
+  switch (cases)
+   {
+
+     case ON_LINE:
+       motorForward();
+       delay(delay_time);
+       break;
+
+     case LINE_ON_LEFT:
+         motorTurn(LEFT);
+         delay(delay_time);
+
+     
+     case LINE_ON_RIGHT:
+         motorTurn(RIGHT);
+         delay(delay_time);
+      }
 }
