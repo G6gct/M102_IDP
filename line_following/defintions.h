f2f95c17cb8470bce9bf5ffee9677c1b29fdbab9
G6gct <gct31@cam.ac.uk>
2023-10-17 22:37:20 +0100

    Initial commit

---
+++ line_following/defintions.h
@@ -0,0 +1,16 @@
+int cases = 0;
+
+// Define different cases
+#define ON_LINE 0
+#define LINE_ON_LEFT 1       
+#define LINE_ON_RIGHT 2
+
+// Defining directions
+#define RIGHT 1
+#define LEFT -1
+
+
+// Importing functions in this file for use in main programme
+void LFSensorRead();
+void LineSensorStart();
+void followLine();
