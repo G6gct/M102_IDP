1319b831e048085778c77b98e587db738277afce
G6gct <gct31@cam.ac.uk>
2023-10-24 11:47:45 +0100

    line following for corner and junctions updated

--- IDP_M102_Main/first_comp_code.ino
+++ IDP_M102_Main/first_comp_code.ino
@@ -1,15 +1,4 @@
 //hard-coded path navigation (go around the perimeter of the grid, detect block, reverse path to start, )
-int delay_time = 50;
-
-
-
-
-void first_comp_path(void) {
-  LFSensorRead(); // calls the sensor reading
-  LineFollowStart();  // calls the line sensors
-  start();
-
-
 
 
 
