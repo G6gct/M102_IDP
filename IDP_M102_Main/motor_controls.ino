9802155354ccb953b943e085fe20b1b1366710f9
G6gct <gct31@cam.ac.uk>
2023-10-31 15:53:49 +0000

    Useless files deleted

--- IDP_M102_Main/motor_controls.ino
+++ IDP_M102_Main/motor_controls.ino
@@ -81,10 +81,8 @@ void Right90(){
 void Left90(){
   delay(400);
   General_Run(-150,255);
-  //Serial.print("HI");
   delay(1200);
   Stop();
-  //Serial.print("Delayed");
 }
 void Turn180(){
   delay(400);
